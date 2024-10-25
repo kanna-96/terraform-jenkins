fa7c077aeb0034e21c60cca1f5220dd90f55abac
tranju664 <164826479+tranju664@users.noreply.github.com>
2024-10-25 14:20:13 +0530

    Update main.tf

--- EC2-Variable/main.tf
+++ EC2-Variable/main.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "terraform-instance-1" {
   ami           = var.my-ami
   instance_type = var.my-instance-type
   key_name      = var.my-key-value
-  count = 4
+  count = 2
   tags = {
     Name  = "${var.my-tag}-${count.index +1}"
     appid = var.app-id
