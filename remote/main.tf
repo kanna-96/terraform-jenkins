ab82b23ee5e257f37b3d3da62e783b9f5534b9fa
kanna-96 <umamahes.201@gmail.com>
2024-10-27 13:53:51 +0530

    Update main.tf

--- remote/main.tf
+++ remote/main.tf
@@ -1,8 +1,11 @@
 provider "aws" {
     region = "ap-south-1"
 assume_role {
-role_arn = "arn:aws:iam::651706759502:role/uma-jenkins-role"
+role_arn = var.my-account
 }
+}
+variable "my-account" {
+default = "asas"
 }
 
 resource "aws_s3_bucket" "terraform-bucket" {
