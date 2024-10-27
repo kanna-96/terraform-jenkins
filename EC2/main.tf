7db9b592883475c6db185a9cbbc73bc5a66afe44
kanna-96 <umamahes.201@gmail.com>
2024-10-27 12:31:24 +0530

    Update main.tf

--- EC2/main.tf
+++ EC2/main.tf
@@ -1,12 +1,14 @@
 provider "aws" {
   region = "ap-south-1"
+assume_role {
 role_arn = "arn:aws:iam::651706759502:role/uma-jenkins-role"
+}
 }
 
 resource "aws_instance" "terraform-instance-1" {
   ami           = "ami-0dee22c13ea7a9a67"
   instance_type = "t2.micro"
-  key_name      = "kk"
+  key_name      = "new-aws-key"
   tags = {
     Name  = "ss-instance-1"
     appid = "345"
